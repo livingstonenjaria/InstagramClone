64710250f09ad4297a738e6ba46449ddcb74a363
Livingstone <livingstonenjaria4@gmail.com>
2018-07-05 14:03:14 +0300

    styling bottom navigation view

--- app/src/main/java/com/example/chauffeur/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/chauffeur/instagramclone/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.chauffeur.instagramclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.chauffeur.instagramclone.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         setupBottomNavigationView();
+    }
+    /**bottom navigation setup **/
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+
     }
 }
---
+++ app/src/main/java/com/example/chauffeur/instagramclone/Utils/BottomNavigationViewHelper.java
@@ -0,0 +1,16 @@
+package com.example.chauffeur.instagramclone.Utils;
+
+import android.util.Log;
+
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class BottomNavigationViewHelper {
+    private static final String TAG = "BottomNavigationViewHel";
+    public static void setupBottomNavigationView(BottomNavigationViewEx bottomNavigationViewEx){
+        Log.d(TAG, "setupBottomNavigationView: Setting up BottomNavigationView");
+        bottomNavigationViewEx.enableAnimation(false);
+        bottomNavigationViewEx.enableItemShiftingMode(false);
+        bottomNavigationViewEx.enableShiftingMode(false);
+        bottomNavigationViewEx.setTextVisibility(false);
+    }
+}
